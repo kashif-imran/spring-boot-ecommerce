2c2fbd28816afd400a34a205b2f6118607d32bae
kashif.emran@gmail.com <kashif.emran@gmail.com>
2022-05-30 22:58:37 +0530

    Enable HTTPS in spring boot app. Now the port will change from 8080 to 8443
    Also disbled CSRF since we are not using cookies for session tracking

--- src/main/java/com/kashif/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/kashif/ecommerce/config/SecurityConfiguration.java
@@ -23,5 +23,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         //force a non-empty response body a 401's to make the response more friendly
         Okta.configureResourceServer401ResponseBody(http);
+
+        //disable CSRF since we are not using cookies for session tracking
+        http.csrf().disable();
     }
 }
