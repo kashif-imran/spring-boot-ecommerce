69cf2474e29bd8efcdf074f234a8ad5833862124
kashif.emran@gmail.com <kashif.emran@gmail.com>
2022-05-30 19:45:17 +0530

    Bug fix add @Configuration to SecurityConfiguration

--- src/main/java/com/kashif/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/kashif/ecommerce/config/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.kashif.ecommerce.config;
 
 import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
